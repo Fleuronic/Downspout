dcf0411f7aef316654c218a725490909d6c0fd14
Jordan Kay <jordan@fleuronic.com>
2024-06-20 22:46:34 -0400

    Update dependencies

--- Modules/Services/Package.swift
+++ Modules/Services/Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:5.10
+// swift-tools-version:6.0
 import PackageDescription
 
 let package = Package(
@@ -12,26 +12,34 @@ let package = Package(
 			targets: [
 				"RaindropService",
 				"RaindropAPI",
+				"RaindropDatabase"
 			]
 		),
 	],
 	dependencies: [
 		.package(name: "Models", path: "../Models"),
 		.package(path: "../../../Dewdrop/Submodules/DewdropAPI"),
-		.package(url: "https://github.com/Fleuronic/Ergo.git", branch: "main")
+		.package(url: "https://github.com/Fleuronic/Ergo.git", branch: "main"),
+		.package(url: "https://github.com/Fleuronic/Catena.git", branch: "main"),
+		.package(url: "https://github.com/Fleuronic/URL", branch: "swift-6"),
+		.package(url: "https://github.com/granoff/Strongbox", from: "0.6.1"),
+		.package(url: "https://github.com/groue/Semaphore", branch: "main")
 	],
 	targets: [
 		.target(
 			name: "RaindropService",
 			dependencies: [
+				"Ergo",
+				"Catena",
+				"Semaphore",
 				.product(name: "Raindrop", package: "Models"),
-				"Ergo"
 			],
 			path: "Sources/Raindrop/Service"
 		),
 		.target(
 			name: "RaindropAPI",
 			dependencies: [
+				"URL",
 				"DewdropAPI",
 				"RaindropService"
 			],
@@ -42,5 +50,19 @@ let package = Package(
 			dependencies: ["RaindropAPI"],
 			path: "Tests/Raindrop/Clients/API"
 		),
-	]
+		.target(
+			name: "RaindropDatabase",
+			dependencies: [
+				"RaindropService",
+				"Strongbox"
+			],
+			path: "Sources/Raindrop/Clients/Database"
+		),
+		.testTarget(
+			name: "RaindropDatabaseTests",
+			dependencies: ["RaindropDatabase"],
+			path: "Tests/Raindrop/Clients/Database"
+		)
+	],
+	swiftLanguageVersions: [.v6]
 )
