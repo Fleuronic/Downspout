0057c67758105f772998e4f23082f1ee4a935043
Jordan Kay <jordan@fleuronic.com>
2024-08-26 14:53:10 -0400

    Switch to Bugsnap

--- Apps/Root App/Resources/main.swift
+++ Apps/Root App/Resources/main.swift
@@ -1,7 +1,16 @@
 import AppKit
+import Bugsnag
 
 import enum Root.Root
 
+@objc(Application) final class Application: NSApplication {
+	// MARK: NSApplication
+	override func reportException(_ exception: NSException) {
+		Bugsnag.notify(exception)
+		super.reportException(exception)
+	}
+}
+
 let delegate = Root.App.Delegate()
-NSApplication.shared.delegate = delegate
+Application.shared.delegate = delegate
 _ = NSApplicationMain(CommandLine.argc, CommandLine.unsafeArgv)
